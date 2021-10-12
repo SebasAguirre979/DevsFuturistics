855079c077dda4b3e01cc8d007efaab93afed44a
jgiraldo <jgiraldozul@gmail.com>
2021-10-11 19:19:26 -0500

    actualizaxion Crear Venta

--- src/paginas/admin/CrearVenta.jsx
+++ src/paginas/admin/CrearVenta.jsx
@@ -1,6 +1,8 @@
+
+import '../../estilos/index.css'
 import React from 'react';
 import { Link } from 'react-router-dom';
-const CrearVenta = () => {
+    const CrearVenta = () => {
     return <div>
       
   
@@ -11,17 +13,40 @@ const CrearVenta = () => {
   <Link to='/admin/ventas'>
   <button className="btn btn-outline-primary mt-3 mb-3 Regresar" > Regresar</button>
   </Link >
+
+  <br />
+  <br />
+            <form className="">
+
+                <div className="col-md-4">
+                    <label for="validationDefault01" className="form-label">Venta #</label>
+                    <input type="number" className="form-control" >
+                    </input>          
+                </div>               
+                <div className="col-md-4">
+                    <label for="validationDefault01" className="form-label">Nombre</label>
+                    <input type="text" className="form-control" >
+                    </input>          
+                </div>
+                <div className="col-md-4">
+                    <label for="number" className="form-label">Cedula</label>
+                    <input type="number" className="form-control" >
+                    </input>          
+                </div>
+                <div className="col-md-4">
+                    <label for="validationDefault01" className="form-label">Fecha</label>
+                    <input type="date" className="form-control" >
+                    </input>          
+                </div>
+            </form>
+  <br />
   <br />
   <br />
   
-  
-  
-  <br />
-  <br />
-  <br />
-  
-  </div>
+
+    </div>
     </div>;
-  };
+    };
+
 export default CrearVenta ;
 
