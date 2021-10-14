7d2c33088e79c919a29e3639fc6f74707dc2ed90
camilo <camiloacevedoesquivel@gmail.com>
2021-10-13 21:08:23 -0500

    union front con back usuarios

--- src/paginas/admin/Clientes.jsx
+++ src/paginas/admin/Clientes.jsx
@@ -1,64 +1,455 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useEffect, useState, useRef } from 'react';
+import { useHistory } from 'react-router';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
 
 const Clientes = () => {
+
+
+  const [usuario, setUsuario] = useState({
+    nombre: '',
+    cedula: '',
+    email: '',
+    pass: '',
+    rol: ''
+  });
+  const form = useRef(null);
+  const [datauser, setDatauser] = useState([]);
+  const [datosUsuariosBackend, setDatosUsuariosBackend] = useState([]);
+  const [cedulaB, setCedulaB] = useState("");
+  const [upduser, setupduser] = useState(false);
+  const [eliminar, setEliminar] = useState(false);
+  const [userdel, setuserdel] = useState({});
+  const [userupdate, setUserupdate] = useState({});
+  const [acutualizarusuario, setacutualizarusuario] = useState(false);
+
+
+
+  useEffect(() => {
+    if (cedulaB !== "") {
+      let users = datosUsuariosBackend.find(buscar => buscar.cedula.match(cedulaB));
+      users !== undefined ? setDatauser([users]) : setDatauser([])
+      console.log(users)
+      if (users !== undefined) {
+        if (cedulaB === users.cedula) {
+          setUsuario({
+            nombre: users.nombre,
+            cedula: users.cedula,
+            email: users.email,
+            pass: users.pass,
+            rol: users.rol
+
+          })
+          toast.info("Usuario encontrado");
+        }
+      }
+
+
+
+    } else {
+      setDatauser(datosUsuariosBackend);
+    }
+
+  }, [cedulaB])
+
+  const buscarUsuario = (e) => {
+    e.preventDefault();
+    setCedulaB(e.target.value);
+  }
+
+  const deluse = async (e) => {
+
+    console.log("Usuario a elminar", userdel)
+
+    const options = {
+      method: 'DELETE',
+      url: 'http://localhost:5000/usuarios/deleteuser',
+      headers: { 'Content-Type': 'application/json' },
+      data: userdel
+    };
+
+    await axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+    setEliminar(false)
+    actualizarDatosback();
+    toast.success("El Usuario  ha sido elminado");
+    setCedulaB("");
+    setUsuario({
+      nombre: "",
+      cedula: "",
+      email: "",
+      pass: "",
+      rol: ""
+
+    })
+  }
+
+  const upduse = async (e) => {
+    console.log("Entro a a ctualizar")
+    console.log("Cambios del usuario", userupdate)
+    const options = {
+      method: 'PATCH',
+      url: 'http://localhost:5000/usuarios/update/',
+      headers: { 'Content-Type': 'application/json' },
+      data: userupdate
+    };
+
+    await axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+
+    actualizarDatosback();
+    toast.success("El Usuario  ha sido actualizado");
+    setCedulaB("");
+    setUsuario({
+      nombre: "",
+      cedula: "",
+      email: "",
+      pass: "",
+      rol: ""
+
+    })
+  }
+
+  function actualizarDatosback() {
+    const options = {
+      method: 'GET',
+      url: 'http://localhost:5000/usuarios/allusers',
+      headers: { 'Content-Type': 'application/json' }
+    };
+
+    axios.request(options).then(function (response) {
+      setDatosUsuariosBackend(response.data);
+      setDatauser(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+
+    console.log("datos de datosusuariosbackend", datosUsuariosBackend);
+    console.log(datauser);
+
+  }
+
+  useEffect(() => {
+    actualizarDatosback();
+  }, [])
+
+  useEffect(async () => {
+    console.log("la actulizacion esta:", acutualizarusuario);
+    if (acutualizarusuario) {
+      console.log("Entro a a ctualizar")
+      console.log("Cambios del usuario", userupdate)
+      const options = {
+        method: 'PATCH',
+        url: 'http://localhost:5000/usuarios/update/',
+        headers: { 'Content-Type': 'application/json' },
+        data: userupdate
+      };
+
+      await axios.request(options).then(function (response) {
+        console.log(response.data);
+      }).catch(function (error) {
+        console.error(error);
+      });
+
+
+      actualizarDatosback();
+      toast.success("El Usuario  ha sido actualizado");
+      setCedulaB("");
+      setUsuario({
+        nombre: "",
+        cedula: "",
+        email: "",
+        pass: "",
+        rol: ""
+
+      })
+    }
+
+
+  }, [upduser])
+
+
+
+
+
+  const usuChange = (e) => {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value
+    })
+    console.log(e.target.name, "  ", e.target.value);
+  }
+
+  const actualizarDatos = () => {
+
+    console.log("Usuario: ", usuario.nombre, "Cedula: ", usuario.cedula);
+    toast.success("Usuario actualizado con éxito")
+  }
+
+
+  const agregarUsuario = (e) => {
+    e.preventDefault();
+    // toast.success("Usuario agregado con éxito")
+
+
+    // setDatauser([...datauser, {
+    //   nombre: usuario.nombre,
+    //   cedula: usuario.cedula,
+    //   email: usuario.email,
+    //   rol: usuario.rol
+    // },]);
+
+    actualizarDatosback();
+
+  };
+
+
+
+  const actualizarUsuario = (e) => {
+    e.preventDefault();
+    var userupdate = false;
+    const fd = new FormData(form.current)
+    const nuevousuario = {};
+    fd.forEach((value, key) => {
+      nuevousuario[key] = value;
+    })
+
+    datosUsuariosBackend.forEach((dato) => {
+      if (dato.cedula === nuevousuario["cedulaB"]) {
+        console.log("se encuentra el usuario para ser actualizado");
+        userupdate = true;
+        setUserupdate(nuevousuario);
+      }
+    })
+
+    if (userupdate) {
+      setacutualizarusuario(true);
+      setupduser(!upduser)
+    } else {
+      toast.error("Usuario no registrado");
+    }
+
+  };
+
+  const eliminarUsuario = (e) => {
+    e.preventDefault();
+    var userdelete = false;
+    var user = "";
+    const fd = new FormData(form.current)
+    const nuevousuario = {};
+    fd.forEach((value, key) => {
+      nuevousuario[key] = value;
+    })
+
+    datosUsuariosBackend.forEach((dato) => {
+      if (dato.cedula === nuevousuario["cedulaB"]) {
+        console.log("se encuentra el usuario para ser borrado");
+        userdelete = true;
+        setuserdel(nuevousuario);
+        // user = dato.nombre;
+      }
+    })
+
+    if (userdelete) {
+      setEliminar(true)
+      // toast.success("El Usuario " + user + " ha sido elminado");
+
+    } else {
+      toast.error("Usuario no registrado");
+    }
+
+  };
+
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    var userregister = true;
+    const fd = new FormData(form.current)
+    const nuevousuario = {};
+    fd.forEach((value, key) => {
+      nuevousuario[key] = value;
+    })
+
+    console.log("Datos del formulario", nuevousuario);
+    console.log("Datos del back", datosUsuariosBackend);
+    //Validacion
+    datosUsuariosBackend.forEach((dato) => {
+      if (dato.email === nuevousuario["email"] || dato.cedula === nuevousuario["cedula"]) {
+        console.log("Usuario ya resgistrado");
+        toast.error("Usuario ya registrado");
+        userregister = false;
+      }
+    })
+
+    if (userregister) {
+
+      const options = {
+        method: 'POST',
+        url: 'http://localhost:5000/usuarios/newuser',
+        headers: { 'Content-Type': 'application/json' },
+        data: nuevousuario
+      };
+
+      await axios.request(options).then(function (response) {
+        console.log(response.data);
+      }).catch(function (error) {
+        console.error(error);
+      });
+      console.log("Usuario agregado con éxito")
+      toast.success("Usuario agregado con éxito");
+      actualizarDatosback();
+      setUsuario({
+        nombre: "",
+        cedula: "",
+        email: "",
+        pass: "",
+        rol: ""
+
+      })
+    }
+
+
+  };
+
   return (
 
 
     <div className="containercam container flex justify-center">
 
-      {/* <div class="text-end">
-        <Link to="/admin">Atras
-        </Link>
 
-      </div> */}
 
       <div className="m-3 text-center text-3xl font-extrabold text-gray-900">
         <h4>Modificación de usuario</h4>
       </div>
 
+      <dialog className=" alert-danger" open={eliminar}>
+        <div className='contdialog'>
+          <h4 className='text-gray-900 text-2xl font-bold'>
+            ¿Está seguro de querer eliminar el usuario?
+          </h4>
+          <div className='contdialogbot'>
+            <button
+              onClick={() => deluse()}
+              className='btn btn-success mx-auto'
+            >
+              Sí
+            </button>
+            <button
+              onClick={() => setEliminar(false)}
+              className='btn btn-danger mx-auto'
+            >
+              No
+            </button>
+          </div>
+        </div>
+      </dialog>
+
 
       <div className="formc col-md-4">
-        <form className="col-md-12  mt-8 max-w-md flex justify-center" novalidate>
+
+
+        <form ref={form} onSubmit={submitForm} className="col-md-12  mt-8 max-w-md flex justify-center" novalidate>
+
+
+          <div className="m-3 text-center text-3xl font-extrabold text-gray-900">
+            <h6>Buscar usuarios</h6>
+          </div>
+
+          <div className="col-md-12">
+
+            <div className="col-md-12">
+              <label for="BuscarCedula" className="form-label">Numero de cedula a buscar</label>
+              <input type="text" className="form-control" id="BuscarCedula"
+                name='cedulaB'
+                value={cedulaB}
+                onChange={buscarUsuario}
+
+              />
+            </div>
+
+          </div>
+
+
+          <div className="m-3 text-center text-3xl font-extrabold text-gray-900">
+            <h6>Agregar o actualizar usuarios</h6>
+          </div>
+
           <div className="col-md-12">
             <label for="username" className="form-label">Nombre Completo</label>
-            <input type="text" className="form-control" id="username" placeholder="debes ingresar el nombre" required />
+            <input type="text" className="form-control" id="username"
+              placeholder="debes ingresar el nombre" required
+              name='nombre'
+              value={usuario.nombre}
+              onChange={usuChange}
+            />
 
           </div>
 
           <div className="col-md-12">
             <label for="validationCustom02" className="form-label">Numero de cedula</label>
-            <input type="text" className="form-control" id="validationCustom02" required />
+            <input type="text" className="form-control" id="validationCustom02" required
+              name='cedula'
+              value={usuario.cedula}
+              onChange={usuChange}
+
+            />
           </div>
           <div className="col-md-12">
             <label for="validationCustomUsername" className="form-label">Email</label>
             <div className="input-group has-validation">
               <span className="input-group-text" id="inputGroupPrepend">@</span>
               <input type="email" className="form-control" id="validationCustomUsername"
-                aria-describedby="inputGroupPrepend" required />
+                aria-describedby="inputGroupPrepend" required
+                name='email'
+                value={usuario.email}
+                onChange={usuChange} />
             </div>
           </div>
           <div className="col-md-12">
             <label for="validationCustom03" className="form-label">Contraseña</label>
-            <input type="password" className="form-control" id="validationCustom03" required />
+            <input type="password" className="form-control" id="validationCustom03" required
+              name='pass'
+              value={usuario.pass}
+              onChange={usuChange}
+
+
+            />
           </div>
           <div className="col-md-9">
             <label for="validationCustom04" className="form-label">Rol</label>
-            <select className="form-select" id="validationCustom04" required>
+            <select name='rol' onChange={usuChange} className="form-select" id="validationCustom04" required>
               <option selected disabled value="">Choose...</option>
-              <option>Administrador</option>
-              <option>Usuario</option>
+              <option value="Administrador">Administrador</option>
+              <option value="Usuario">Usuario</option>
             </select>
           </div>
           <div className="contbotones col-md-12 " >
-            <button className="btn btn-primary " type="submit">Actualizar</button>
-            <button className="botone btn btn-primary" id="botonEliminar" type="submit">Eliminar</button>
-            <Link to="/admin"><button className="botone btn btn-primary" id="botonCancelar" type="submit">Cancelar</button>
+            <button className="btn btn-success mx-auto" type="submit" name='botonAgregar'  >Agregar</button>
+            <button className="btn btn-primary mx-auto " type="button" name='botonActualizar' onClick={actualizarUsuario}    >Actualizar</button>
+            <button className="btn btn-danger mx-auto"  id="botonEliminar" type="button" onClick={eliminarUsuario} >Eliminar</button>
+            
+          </div>
+          <div className="contbotones2 col-md-12 " >
+            <Link to="/admin"><button className="btn btn-dark mx-auto" id="botonCancelar" type="button">Atrás</button>
             </Link>
 
           </div>
         </form>
-
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000} />
       </div>
 
       <div className="contTabla col-md-7 ">
@@ -73,25 +464,27 @@ const Clientes = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              {/* <th scope="row">1</th> */}
-              <td>Camilo Andrés Acevedo</td>
-              <td>1234567890</td>
-              <td>cacevedol@gmail.com</td>
-              <td>Administrador</td>
-            </tr>
-            <tr>
-              {/* <th scope="row">2</th> */}
-              <td>Jacob</td>
-              <td>78945621352</td>
-              <td>ddddcdcd@fddat</td>
-              <td>Usuario</td>
-            </tr>
-            <tr>
-            </tr>
+            {
+              datauser.map((usr) => {
+                return (
+                  <tr>
+                    <td>{usr.nombre}</td>
+                    <td>{usr.cedula}</td>
+                    <td>{usr.email}</td>
+                    <td>{usr.rol}</td>
+                  </tr>
+                )
+
+              }
+              )
+            }
           </tbody>
         </table>
       </div>
+
+
+
+
     </div>
 
 
