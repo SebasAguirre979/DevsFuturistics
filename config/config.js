393f74002a3f2c1c0d3736f306aca07df5b06d40
Sebastián Aguirre <sebastian.aguirre.rodriguez@correounivalle.edu.co>
2021-10-24 11:55:32 -0500

    SA eliminando algunas variables config

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 
 module.exports = {
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "root",
-    database: process.env.DB_DATABASE || "devsfuturistics",
+    username: "sql10446405" || "root",
+    password: "QPYbM5iHFK" || "root",
+    database: "sql10446405" || "devsfuturistics",
     host: process.env.DB_HOST || "127.0.0.1",
-    dialect: process.env.DB_DIALECT || "mysql",
+    dialect: "mysql",
     /* define: {
       timestamps: false,
 
